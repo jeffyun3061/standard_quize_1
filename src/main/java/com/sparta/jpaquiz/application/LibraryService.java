437adc79cf11084758cdd7aca9798269b41152c0
마리 <audgh3364@gmail.com>
2024-12-11 18:35:56 -0800

    주석 업데이트, 디렉토리 구조 변경

--- src/main/java/com/sparta/jpaquiz/application/LibraryService.java
+++ src/main/java/com/sparta/jpaquiz/application/LibraryService.java
@@ -18,16 +18,17 @@ public class LibraryService {
         this.categoryRepository = categoryRepository;
     }
 
+    /**
+     * TODO 5: Persistence Context와 @Transactional을 활용해 Category와 Book을 저장합니다.
+     * 처리 순서:
+     * 1. categoryName 으로 새로운 Category(category1) 를 생성합니다.
+     * 2. bookTitle1, bookTitle2 로 새로운 Book(book1, book2) 을 생성합니다.
+     * 3. book1, book2을 category1 과 연결합니다. *DB의 외래 키 값을 업데이트하기 위해 Owning 사이드(Book)에 추가
+     * 4. category1과 book1, book2 을 연결합니다. *메모리내 객체 상태 동기화를 위해 Non-Owning 사이드(Category)에도 추가
+     * 5. category1 저장 *Cascade 설정을 활용해 Category 저장 시 Book도 함께 저장
+     **/
     @Transactional
     public void addCategoryWithBooks(String categoryName, String bookTitle1, String bookTitle2) {
-        // TODO 5: Persistence Context와 @Transactional을 활용해 Category와 Book을 저장합니다.
-        // 처리 순서:
-        // 1. categoryName 으로 새로운 Category(category1) 를 생성합니다.
-        // 2. bookTitle1, bookTitle2 로 새로운 Book(book1, book2) 을 생성합니다.
-        // 3. book1, book2을 category1 과 연결합니다. *DB의 외래 키 값을 업데이트하기 위해 Owning 사이드(Book)에 추가
-        // 4. category1과 book1, book2 을 연결합니다. *메모리내 객체 상태 동기화를 위해 Non-Owning 사이드(Category)에도 추가
-        // 5. category1 저장 *Cascade 설정을 활용해 Category 저장 시 Book도 함께 저장
-
         // 1. categoryName 으로 새로운 Category(category1) 를 생성합니다.
         Category category = new Category();
         ...
